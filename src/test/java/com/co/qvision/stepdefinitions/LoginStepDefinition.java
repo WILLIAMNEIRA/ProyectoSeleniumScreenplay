9987db25a0cc75c99121d52361a255cad2ba45d5
WILLIAMNEIRA <wamonroyn@ufpso.edu.co>
2022-11-18 21:03:45 -0500

    creacion de los features

---
+++ src/test/java/com/co/qvision/stepdefinitions/LoginStepDefinition.java
@@ -0,0 +1,26 @@
+package com.co.qvision.stepdefinitions;
+
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+public class LoginStepDefinition {
+    @Given("^he user is on the page$")
+    public void heUserIsOnThePage() {
+
+    }
+
+    @When("^he user enter credentials$")
+    public void heUserEnterCredentials() {
+
+    }
+
+    @Then("^he user his login$")
+    public void heUserHisLogin() {
+
+    }
+
+
+
+
+}
---
+++ src/test/resources/features/Login.feature
@@ -0,0 +1,7 @@
+Feature: login
+  I as service user , i need login
+
+  Scenario: login succesfull
+    Given  he user is on the page
+    When he user enter credentials
+    Then  he user his login
